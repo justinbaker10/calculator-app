fc8e5836bd1f632604068979cbba223b31266572
justinbaker10 <justin-baker@live.com>
2020-06-05 14:15:24 -0500

    built calculator structure and style

---
+++ script.js
@@ -0,0 +1,12 @@
+class Calculator {
+    
+}
+
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const clearButton = document.querySelector('[data-clear]')
+const previousOperand = document.querySelector('[data-previous-operand]')
+const currentOperand = document.querySelector('[data-current-operand]')
